5b03d0476fe426b2dbd70b609ed2a635f666e0c2
Shane <shaneboo152@gmail.com>
2021-10-27 21:30:03 -0700

    development

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import '../../App.css'
+import Cards from '../cards/Cards'
+import HeroSection from '../hero-section/HeroSection'
+
+
+function Home(){
+return (
+
+    <div>
+        <HeroSection/>
+        <Cards/>
+    </div>
+)
+
+}
+
+export default Home
